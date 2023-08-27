18353235a78266dd15ac1c8736d892362e842a7b
Ds258 <ds25082002@gmail.com>
2023-08-27 23:16:31 +0700

    Configure database

--- backend/rest_api/models.py
+++ backend/rest_api/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=100)
-    profile_pic = models.CharField(max_length=100, unique=True)
-    DOB = models.DateField()
+    username = models.CharField(max_length=100, unique=True)
+    profile_pic = models.CharField(max_length=100, null=True)
+    DOB = models.DateField(null=True)
     email = models.EmailField()
     password = models.CharField()
     privilege = models.BooleanField()
-    create_date = models.DateTimeField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True, null=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Navbar from './components/common/Navbar/Navbar';
 import Slider from './components/homepage/Slider/Slider';
 import WN from './components/homepage/WN/WN';
 import Login from './pages/Login/Login';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 function App() {
   return (
