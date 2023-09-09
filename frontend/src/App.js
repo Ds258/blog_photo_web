961cd2cc7b44f08466dd6993102be5255d395bea
Ds258 <ds25082002@gmail.com>
2023-09-09 22:00:26 +0700

    Create sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,8 @@ import Navbar from './components/common/Navbar/Navbar';
 import Slider from './components/homepage/Slider/Slider';
 import WN from './components/homepage/WN/WN';
 import Login from './pages/Login/Login';
-import {BrowserRouter, Route} from 'react-router-dom';
+// import {BrowserRouter, Route} from 'react-router-dom';
+import Signup from './pages/Sign_up/Sign_up';
 
 function App() {
   return (
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import "./Login.css"
 
 export default function Login() {
-    axios.post('/login', {
-        
-    });
 
     return (
         <div className="text-center align-items-center login">
             <main className="form-signin">
                 <form>
-                    <h1 className="h3 mb-3 fw-normal">Sign In</h1>
+                    <h1 className="h3 mb-3 fw-normal">Sign in</h1>
                     <div className="form-floating email">
                         <input type="email" className="form-control" id="floatingEmail" placeholder="name@example.com" />
                         <label htmlFor="floatingEmail">Email Address</label>
@@ -26,7 +23,7 @@ export default function Login() {
                     </div>
                 </form>
                 <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-                <h3 className="h6 mt-3 mb-2 fw-normal">Not a member yet? Sign up now</h3>
+                <h3 className="h6 mt-3 mb-2 fw-bold">Not a member yet? Sign up now</h3>
                 <button className="w-100 btn btn-lg btn-danger" type="submit">Sign up</button>
                 <p className="mt-5 mb-3 copyright">© 2023</p>
             </main>
